168537c437a906fd2fe60ceb072e8b4ce2f38e4c
giulianogiova20 <94998931+giulianogiova20@users.noreply.github.com>
2022-09-16 20:38:47 -0300

    Footer ready for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export const Footer = () => {
     
     return (
         <>
-            <div className="footer">
+            <Container fluid className="footer">
                 <Row className="p-5">
                     <Col md={4} sm={12} className="footer-section1 px-5 py-4">
                         <div >
@@ -22,7 +22,7 @@ export const Footer = () => {
                         </div>
                         <p className="text-center text-md-start">Los mejores cortes de carne y los mojitos más deliciosos de todo Tepoztlán.</p>
                     </Col>
-                    <Col md={4} sm={12} className="footer-section2 px-md-4 px-lg-5 py-4">
+                    <Col md={4} sm={12} className="footer-section2 px-5 py-4">
                         <div className="d-flex justify-content-md-center">
                             <div>
                                 <h3 className="text-light">CONTACTO</h3>
@@ -50,35 +50,37 @@ export const Footer = () => {
                             </div>
                         </div>
                     </Col>
-                    <Col md={4} sm={12} className="footer-section3 px-md-4 px-lg-5 py-4">
-                        <div className="d-flex justify-content-md-center">
+                    <Col md={4} sm={12} className="footer-section3 px-5 py-4">
+                        <div className="">
                             <div>
                                 <h3 className="text-light">HORARIO</h3>
-                                <dl>
-                                    <dt>Jueves </dt>
-                                    <dd> 11:00 - 20:00</dd>
-                                    <dt>Viernes </dt>
-                                    <dd> 11:00 - 20:00</dd>
-                                    <dt>Sábado </dt>
-                                    <dd> 11:00 - 21:00</dd>
-                                    <dt>Domingo </dt>
-                                    <dd> 11:00 - 21:00</dd>
-                                </dl>
+                                <Row className='d-flex justify-content-between'>
+                                    <dl>
+                                        <dt>Jueves </dt>
+                                        <dd> 11:00 - 20:00</dd>
+                                        <dt>Viernes </dt>
+                                        <dd> 11:00 - 20:00</dd>
+                                        <dt>Sábado </dt>
+                                        <dd> 11:00 - 21:00</dd>
+                                        <dt>Domingo </dt>
+                                        <dd> 11:00 - 21:00</dd>
+                                    </dl>
+                                </Row>
                             </div>
                         </div>
                     </Col>
                 </Row>
                 <Row className="p-5 footer-map">
-                    <Row className="justify-content-center px-5">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4543.572305580706!2d-99.10254159605253!3d18.98879462455438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce0df76ef765e3%3A0x399c4ff048fe4ff9!2sThe%20Sound%20of%20Grill!5e0!3m2!1ses-419!2sar!4v1662766869479!5m2!1ses-419!2sar"></iframe>
-                    </Row>
+                    <Col className="justify-content-center px-5">
+                        <iframe className='w-100 h-100' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4543.572305580706!2d-99.10254159605253!3d18.98879462455438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce0df76ef765e3%3A0x399c4ff048fe4ff9!2sThe%20Sound%20of%20Grill!5e0!3m2!1ses-419!2sar!4v1662766869479!5m2!1ses-419!2sar"></iframe>
+                    </Col>
                 </Row>
-                <Row className="footer-info">
+                <Row className=" footer-info">
                     <Row className="justify-content-center align-items-center">
                         <p className="text-center">© 2022 The Sound of Grill - Diseño y Desarrollo</p>
                     </Row>
                 </Row>
-            </div>
+            </Container>
         </>
     )
 }
