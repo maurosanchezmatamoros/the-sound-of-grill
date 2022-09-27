a8dc7a08c9b0c2ee0673833a7961d2b39f6fb589
giulianogiova20 <94998931+giulianogiova20@users.noreply.github.com>
2022-09-27 00:32:10 -0300

    Added modal - test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,10 @@
 import TheSoundOfGrill from "./icons/TheSoundOfGrill"
 import '../styles/NavbarMenu.css'
 import { useState } from "react"
+import { ModalReserva} from "./Reserva"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 export const Navbar = () => {
 
@@ -15,6 +19,12 @@ export const Navbar = () => {
     }
 
     window.addEventListener("scroll", handleScroll)
+
+    const [show, setShow] = useState(false);
+      
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+      
 
     return(
         <nav className={navbarBack? "navbarMenu navbarBack" : "navbarMenu"}>
@@ -32,8 +42,41 @@ export const Navbar = () => {
                 </li>
                 <li className="menuItem">Conocenos</li>
                 <li className="menuItem">Contacto</li>
-                <li className="menuItem reservar">RESERVAR</li>
+                <li className="menuItem reservar" onClick={handleShow}>RESERVAR</li>
             </ul>
+      
+            <Modal show={show} onHide={handleClose} size="sm">
+              <Modal.Header closeButton>
+                <Modal.Title>Haga su reserva</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="name@example.com"
+                      autoFocus
+                    />
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                  >
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
         </nav>
     )
 }
