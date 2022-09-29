ab9a86864d6c983b86d5551604c67ba7357aef9e
Mauro Sanchez Matamoros <maurosanchezmatamoros@gmail.com>
2022-09-29 15:14:49 -0300

    Menu Mobile refactored

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import '../styles/NavbarMenu.css'
 import { useContext, useState } from "react"
 import { Link } from 'react-scroll'
 import { categoryContext } from "../App"
+import { NavbarMobile } from "./index"
 
 export const Navbar = () => {
 
@@ -54,24 +55,12 @@ export const Navbar = () => {
                     <li className="menuItem reservar">RESERVAR</li>
                 </ul>
             </div>
-            {/* ------------------------------MOBILE---------------------------------- */}
-            <ul className={collapseMenu === false? 'menuMobileHidden' : 'menuMobile'}>
-                <Link to="inicio" spy={true} smooth={true} offset={-50} duration={200} onClick={() => setCollapseMenu(false)}><li className="menuItemMobile">Inicio</li></Link>
-                <li className="menuItemMobile" onClick={() => setDisplayMenu(!displayMenu)}>
-                    Menú <span className="material-symbols-outlined">expand_more</span>
-                </li>
-                {displayMenu? <ul className="dropMenuMobile">
-                    <Link to='menu' spy={true} smooth={true} offset={-60} duration={100} onClick={() => handleOnClick('destacados')}><li>Destacados</li></Link>
-                    <Link to='menu' spy={true} smooth={true} offset={-60} duration={100} onClick={() => handleOnClick('platos')}><li>Platos</li></Link>
-                    <Link to='menu' spy={true} smooth={true} offset={-60} duration={100} onClick={() => handleOnClick('postres')}><li>Postres</li></Link>
-                    <Link to='menu' spy={true} smooth={true} offset={-60} duration={100} onClick={() => handleOnClick('bebidas')}><li>Bebidas</li></Link>
-                </ul> : null}
-                <Link to="conocenos" spy={true} smooth={true} offset={-50} duration={200} ><li className="menuItemMobile" onClick={() => setCollapseMenu(false)}>Conocenos</li></Link>
-                <Link to="contacto" spy={true} smooth={true} offset={-50} duration={200} ><li className="menuItemMobile" onClick={() => setCollapseMenu(false)}>Contacto</li></Link>
-                <div className="menuItemMobile">
-                    <li className="reservarMobile" onClick={() => setCollapseMenu(false)}>RESERVAR</li>
-                </div>
-            </ul>
+            <NavbarMobile
+            collapseMenu={collapseMenu}
+            setCollapseMenu={setCollapseMenu}
+            displayMenu={displayMenu}
+            setDisplayMenu={setDisplayMenu}
+            handleOnClick={handleOnClick} />
         </nav>
     )
 }
---
+++ src/components/NavbarMobile.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-scroll'
+import '../styles/NavbarMobile.css'
+
+export const NavbarMobile = ({ collapseMenu, setCollapseMenu, displayMenu, setDisplayMenu, handleOnClick }) => {
+    return(
+        <ul className={collapseMenu === false? 'menuMobileHidden' : 'menuMobile'}>
+            <Link to="inicio" spy={true} smooth={true} offset={-50} duration={200} onClick={() => setCollapseMenu(false)}><li className="menuItemMobile">Inicio</li></Link>
+            <li className="menuItemMobile" onClick={() => setDisplayMenu(!displayMenu)}>
+                Menú <span className="material-symbols-outlined">expand_more</span>
+            </li>
+            {displayMenu? <ul className="dropMenuMobile">
+                <Link to='menu' spy={true} smooth={true} offset={-60} duration={100} onClick={() => handleOnClick('destacados')}><li>Destacados</li></Link>
+                <Link to='menu' spy={true} smooth={true} offset={-60} duration={100} onClick={() => handleOnClick('platos')}><li>Platos</li></Link>
+                <Link to='menu' spy={true} smooth={true} offset={-60} duration={100} onClick={() => handleOnClick('postres')}><li>Postres</li></Link>
+                <Link to='menu' spy={true} smooth={true} offset={-60} duration={100} onClick={() => handleOnClick('bebidas')}><li>Bebidas</li></Link>
+            </ul> : null}
+            <Link to="conocenos" spy={true} smooth={true} offset={-50} duration={200} ><li className="menuItemMobile" onClick={() => setCollapseMenu(false)}>Conocenos</li></Link>
+            <Link to="contacto" spy={true} smooth={true} offset={-50} duration={200} ><li className="menuItemMobile" onClick={() => setCollapseMenu(false)}>Contacto</li></Link>
+            <div className="menuItemMobile">
+                <li className="reservarMobile" onClick={() => setCollapseMenu(false)}>RESERVAR</li>
+            </div>
+        </ul>
+    )
+}
