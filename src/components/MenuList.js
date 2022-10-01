e594ff4f5b5fb312679af3290614d89ca189370e
giulianogiova20 <94998931+giulianogiova20@users.noreply.github.com>
2022-10-01 17:32:22 -0300

    menu download pdf added

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -51,6 +51,41 @@ export const MenuList = ({ category, setCategory }) => {
         getItems()
     }, [category])
 
+    const menuDescargarData = async () =>{
+        try {
+            await fetch(process.env.PUBLIC_URL + 'Menu_SOG.pdf', {
+                method: 'GET',
+                headers: {
+                'Content-Type': 'application/pdf',
+                },
+            })
+            .then((response) => response.blob())
+            .then((blob) => {
+                // Create blob link to download
+                const url = window.URL.createObjectURL(
+                new Blob([blob]),
+                );
+                const link = document.createElement('a')
+                link.href = url;
+                link.setAttribute(
+                'download',
+                `Menu_SOG.pdf`,
+                )
+    
+                // Append to html link element page
+                document.body.appendChild(link);
+    
+                // Start download
+                link.click()
+    
+                // Clean up and remove the link
+                link.parentNode.removeChild(link)
+            })
+        } catch {
+            setError(true)
+        }        
+    }
+
     return(
         <section id='menu'>
             <Row className="menuContainer gx-0 justify-content-center">
@@ -80,7 +115,7 @@ export const MenuList = ({ category, setCategory }) => {
                     )
                 )
                 }
-                <button className="mt-5 descargarMenu">DESCARGAR MENÚ</button>
+                <button className="mt-5 descargarMenu" onClick={menuDescargarData}>DESCARGAR MENÚ</button>
             </Row>
         </section>
     )
